62d9ff8aad276eee38da9eab7f805704ee0c0191
sylwiagoj <sylwia.k.goj@gmail.com>
2024-07-23 16:30:50 +0200

    Update SQLzoo-8-USING Null.sql

--- SQLZOO- Tasks and Solutions/SQLzoo-8-USING Null.sql
+++ SQLZOO- Tasks and Solutions/SQLzoo-8-USING Null.sql
@@ -36,7 +36,7 @@
 --6. Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
 
 	SELECT teacher.name AS teacher_name, 
-    COALESCE(dept.name, 'None') AS dept_name
+    	COALESCE(dept.name, 'None') AS dept_name
 	FROM teacher
 	LEFT JOIN dept
 	ON teacher.dept = dept.id;
